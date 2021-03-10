72434491f5072bd55dfddb216f22062dd1eb6fc6
Larry Gamboa <techmedia.lg@gmail.com>
2021-03-10 11:42:22 -0800

    set up server.js

--- server.js
+++ server.js
@@ -1,18 +1,34 @@
+// required dependencies
 const express = require("express");
-const path = require("path");
+// const path = require("path");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+// define the PORT
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// define middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+// define API routes
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+  useNewUrlParser: true
+});
+
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
