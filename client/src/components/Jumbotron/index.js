0cef6e4ef9ec43432cac36e9d67ffc43181263b6
Larry Gamboa <techmedia.lg@gmail.com>
2021-03-10 20:18:21 -0800

    create nav bar and jumbotron components

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import style from "./style.css";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron jumbotron-fluid text-center">
+            <div className="container">
+                <h1 className="display-4">Google Books Search</h1>
+                <p className="lead">Search and save books of interest.</p>
+            </div>
+        </div>
+    );
+}
+
+export default Jumbotron;
