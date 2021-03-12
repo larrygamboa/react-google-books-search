cc007fa1ebe285fae6c7c9f51ba2f32502976956
Larry Gamboa <techmedia.lg@gmail.com>
2021-03-11 19:24:29 -0800

    upload a bunch of stuff

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
+import SearchForm from "./components/SearchForm";
+import Wrapper from "./components/Wrapper";
+import NoMatch from "./pages/NoMatch";
+import Search from "./pages/Search";
+import Saved from "./pages/Saved";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <Nav />
+          <Jumbotron />
+          <SearchForm />
+          <Wrapper>
+            
+          </Wrapper>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
+
 
 export default App;
